1be9599b23fa81e0a0d6a444f282341a9ae09a30
fofl12 <70167176+fofl12@users.noreply.github.com>
2022-08-02 17:56:55 +0000

    o2

--- src/rssreader.lua
+++ src/rssreader.lua
@@ -1,3 +1,34 @@
+function getChatColor(user)
+    local CHAT_COLORS =
+    {
+        Color3.new(253/255, 41/255, 67/255), -- BrickColor.new("Bright red").Color,
+        Color3.new(1/255, 162/255, 255/255), -- BrickColor.new("Bright blue").Color,
+        Color3.new(2/255, 184/255, 87/255), -- BrickColor.new("Earth green").Color,
+        BrickColor.new("Bright violet").Color,
+        BrickColor.new("Bright orange").Color,
+        BrickColor.new("Bright yellow").Color,
+        BrickColor.new("Light reddish violet").Color,
+        BrickColor.new("Brick yellow").Color,
+    }
+
+        local function GetNameValue(pName)
+            local value = 0
+            for index = 1, #pName do
+                local cValue = string.byte(string.sub(pName, index, index))
+                local reverseIndex = #pName - index + 1
+                if #pName%2 == 1 then
+                    reverseIndex = reverseIndex - 1
+                end
+                if reverseIndex%4 >= 2 then
+                    cValue = -cValue
+                end
+                value = value + cValue
+            end
+            return value
+        end
+
+    return '#' .. CHAT_COLORS[(GetNameValue(user) % #CHAT_COLORS) + 1]:ToHex()
+end
 function newParser()
 
     XmlParser = {};
@@ -152,23 +183,53 @@ end
 local xml = newParser()
 
 local feeds = {
-	'https://www.youtube.com/feeds/videos.xml?channel_id=UCXuqSBlHAE6Xw-yeJA0Tunw',
+	'https://raw.githubusercontent.com/fofl12/sc/main/src/feed.rss',
 	'https://archlinux.org/feeds/news/',
 }
 local http = game:GetService('HttpService')
 local items = {}
+local months = {
+	'Jan',
+	'Feb',
+	'Mar',
+	'Apr',
+	'May',
+	'Jun',
+	'Jul',
+	'Aug',
+	'Sep',
+	'Oct',
+	'Nov',
+	'Dec'
+}
 for _, feed in next, feeds do
 	local doc = xml:ParseXmlText(http:GetAsync(feed))
 	for _, item in next, doc.rss.channel:children() do
 		if item.title then
+			print(item.title:value(), '-----')
+			local date = item.pubDate:value():split(" ")
+			print(item.pubDate:value())
+			local day = tonumber(date[2])
+			local month = table.find(months, date[3]:sub(1, 3))
+			print(month)
+			local year = tonumber(date[4])
+			local time = date[5]:split(':')
+			local realdate = DateTime.fromUniversalTime(
+				year, month, day,
+				tonumber(time[1]), tonumber(time[2]), tonumber(time[3])
+			).UnixTimestamp
 			table.insert(items, {
 				name = doc.rss.channel.title:value(),
 				title = item.title:value(),
-				desc = item.description:value()
+				desc = item.description:value(),
+				date = realdate
 			})
 		end
 	end
 end
+table.sort(items, function(a, b)
+	return a.date > b.date
+end)
 
 local board = Instance.new('Part')
 board.Position = owner.Character.Head.Position + Vector3.new(0, 2)
@@ -204,7 +265,7 @@ chatText.TextSize = 16
 
 local function sendMessage(text, author)
 	local vis = chatText:Clone()
-	vis.Text = ('<font color="#ff0000">[%s]</font> %s'):format(author, text)
+	vis.Text = ('<font color="%s">[%s]</font> %s'):format(getChatColor(author), author, text)
 	vis.Name = os.time()
 	vis.Parent = chat
 
@@ -227,5 +288,5 @@ local function sendMessage(text, author)
 end
 
 for _, item in ipairs(items) do
-	sendMessage(item.title .. '<br/>' .. item.desc, item.name)
+	sendMessage('<b>' .. item.title .. '</b><br/>' .. item.desc, item.name)
 end
